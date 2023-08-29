a89121c517caae102a4aededa523d8ee96c7d6a2
Chanyoung Park <chanyoung.p.1988@gmail.com>
2023-08-29 06:03:33 -0600

    Streamlit app File upload Complete

--- streamlit_app.py
+++ streamlit_app.py
@@ -224,8 +224,8 @@ def get_listing_info():
             df_sale_listings.columns = [col.replace(
                 'hdpData.homeInfo.', '') for col in df_sale_listings.columns]
 
-            duplicated_columns = df_sale_listings.columns[df_sale_listings.columns.duplicated(
-                keep='first')]
+            duplicated_columns = [col for col in df_sale_listings.columns if col.endswith(
+                '.1') or col.endswith('.2')]
             df_sale_listings = df_sale_listings.drop(
                 columns=duplicated_columns)
 
