cc417bed4cee2439ded2cb937d069ac6d1a9b8c1
Chanyoung Park <chanyoung.p.1988@gmail.com>
2023-08-29 02:37:56 -0600

    Streamlit app Property Details

--- streamlit_app.py
+++ streamlit_app.py
@@ -329,11 +329,11 @@ def get_property_info():
     if st.button("Run", type="secondary"):
         result = get_properties(
             api_key=api_key, email=email, zpid=zpid, address=address)
-        # st.write(result.json()['is_success'])
-        if result.json()['is_success']:
-            # st.write(result.json()['data'])
-            df_prop = pd.json_normalize(result.json()['data'])
-            st.write(df_prop)
+        st.write(json.dumps(result.json()['data'], indent=4))
+        # if result.json()['is_success']:
+        #     # st.write(result.json()['data'])
+        #     df_prop = pd.json_normalize(result.json()['data'])
+        #     st.write(df_prop)
 
         #     data_for_mongo = {
         #         "description": "Listing data for ObjectId generation"}
