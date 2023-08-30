6cfaaf55a17e034a015f210afa68d4279e928b2f
Chanyoung Park <chanyoung.p.1988@gmail.com>
2023-08-30 00:21:22 -0600

    Streamlit app File Analystics

--- streamlit_app.py
+++ streamlit_app.py
@@ -432,6 +432,71 @@ def data_analystic():
                 return
             st.write(df)
 
+            # If prefix is properties, apply main functionalities from app.py
+            if prefix == 'properties':
+                #####################################
+                #              METRICS              #
+                #####################################
+                st.markdown("## Property Metrics 🏙️")
+                col1, col2, col3, col4 = st.columns(4)
+
+                df['price'] = df['price'].apply(clean_price)
+                df = df.dropna(subset=['price'])
+
+                col1.metric('Total',  len(df))
+                col2.metric('Avg Price', "${:,}".format(
+                    int(df['price'].mean())).split(',')[0] + 'K')
+                col3.metric('Avg DOM', int(df['DAYS ON MARKET'].mean()))
+                col4.metric('Avg PPSQFT', "${:,}".format(
+                    int(df['$/SQUARE FEET'].mean())))
+
+                #####################################
+                #             CHARTS                #
+                #####################################
+                with st.expander('Charts', expanded=True):
+                    fig = px.histogram(df, x="DAYS ON MARKET",
+                                       title="Days on Market Histogram Chart")
+                    st.plotly_chart(fig, use_container_width=True)
+                    fig = px.box(df, x="PRICE", title="Price Box Plot Chart")
+                    st.plotly_chart(fig, use_container_width=True)
+                    fig = px.histogram(df, x="$/SQUARE FEET",
+                                       title="Price per SQFT Histogram Chart")
+                    st.plotly_chart(fig, use_container_width=True)
+
+                #####################################
+                #             FEATURES              #
+                #####################################
+                df_features = df.copy()
+                df_features['ratio_sqft_bd'] = df_features['SQUARE FEET'] / \
+                    df_features['BEDS']
+                df_features['additional_bd_opp'] = df_features.apply(
+                    lambda x: additional_bedroom_opportunity(x), axis=1)
+                df_features['ratio_lot_sqft'] = df_features['LOT SIZE'] / \
+                    df_features['SQUARE FEET']
+                df_features['adu_potential'] = df_features.apply(
+                    lambda x: adu_potential(x), axis=1)
+
+                #####################################
+                #              TABLES               #
+                #####################################
+                with st.expander('Opportunities', expanded=True):
+                    df_add_bd = df_features.loc[df_features['additional_bd_opp'] == True]
+                    df_adu = df_features.loc[df_features['adu_potential'] == True]
+
+                    col1, col2 = st.columns(2)
+                    col1.metric('Total Add Bd', len(df_add_bd))
+                    col2.metric('Total ADU', len(df_adu))
+
+                    st.write(df_features)
+
+                    csv = df_features.to_csv(index=False)
+                    st.download_button(
+                        label="Download 🔽",
+                        data=csv,
+                        file_name=f"{selected_file}_features.csv",
+                        mime="text/csv"
+                    )
+
             csv = df.to_csv(index=False)
             st.download_button(
                 label="Download 🔽",
