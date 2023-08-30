d74f8d87a1cb5cb5149ce8ccc101e416698df65f
Chanyoung Park <chanyoung.p.1988@gmail.com>
2023-08-30 02:00:40 -0600

    Refactor: Extract modules/functions from streamlit_app

--- components/property_detail.py
+++ components/property_detail.py
@@ -7,14 +7,20 @@ from function.functions import *
 
 def get_property_info():
 
+    # Set the title for the Streamlit app
     st.title("Property Detail Search 🔍")
+
+    # Check if session states are initialized, if not, set them
     if "visibility" not in st.session_state:
         st.session_state.visibility = "visible"
         st.session_state.disabled = False
         st.session_state.placeholder = "Enter value"
 
+    # Create a Streamlit container for user input
     with st.container():
         st.markdown("## 1. Enter a Unique Identifier 🏠")
+
+        # Get zpid input from the user
         zpid = st.text_input(
             'Unique ID',
             # label_visibility=st.session_state.visibility,
@@ -22,7 +28,11 @@ def get_property_info():
             # placeholder='1234567'
             '2078133107'
         )
+
+        # Offer an alternative input method for address
         st.text('or')
+
+        # Get address input from the user
         address = st.text_input(
             'Full Address',
             label_visibility=st.session_state.visibility,
@@ -30,8 +40,11 @@ def get_property_info():
             placeholder='110 1st St, Jersey City, NJ 07302'
         )
 
+    # Create another Streamlit container for API key input
     with st.container():
         st.markdown("## 2. Enter your API Key 👇")
+
+        # Get API key input from the user
         api_key = st.text_input(
             'API Key',
             label_visibility=st.session_state.visibility,
@@ -40,29 +53,38 @@ def get_property_info():
             type="password"
         )
 
+    # Check if user has clicked the "Run" button
     if st.button("Run", type="secondary"):
+
+        # Call function to get properties using provided inputs
         result = get_properties(api_key=api_key, zpid=zpid, address=address)
 
+        # Check if the API call was successful
         if result.json()['is_success']:
+
+            # Parse the JSON response to a DataFrame
             data = result.json()['data']
             df_prop = pd.json_normalize(data)
             for col in df_prop.columns:
                 df_prop[col] = df_prop[col].apply(lambda x: str(
                     x) if isinstance(x, list) or isinstance(x, dict) else x)
 
+            # Preprocess the obtained DataFrame
             df_prop = preprocess_dataframe(df_prop)
 
+            # Save the data to the database and get relevant info
             data_for_mongo = {
                 "description": "Property data for ObjectId generation"}
             object_id, filename = properties_save_to_db(data_for_mongo, zpid)
 
-            # GCS connect
+            # Connect to Google Cloud Storage
             storage_client = gcs_connect()
 
-            # GCS Blob Storage에 파일을 저장
+            # Upload the DataFrame as CSV to GCS
             df_prop.to_csv(filename, index=False)
             file_upload_to_gcs(filename, storage_client, prefix='properties')
 
+            # Show success message to user
             st.markdown(
                 f"""
                 Successfully retrieved data! Go to the analytics tab to view results.
