fd23c7a6ce8da7041e91f6cf67c94c6dbcb4b256
Chanyoung Park <chanyoung.p.1988@gmail.com>
2023-08-29 23:03:35 -0600

    Streamlit app File upload Complete

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import io
 import json
 import base64
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from pymongo import MongoClient
 from google.cloud import storage
 from urllib.error import URLError
@@ -84,12 +84,15 @@ def listings_save_to_db(data):
     db = client[DB_NAME]
     collection = db[COLLECTION_NAME]
 
-    result = collection.insert_one(data)
-    object_id = result.inserted_id
-
     today = datetime.today().strftime('%Y-%m-%d')
     filename = f"{today}-{object_id}.csv"
     data['file'] = filename
+
+    data['createAt'] = datetime.now()
+    data['expireAt'] = datetime.now() + timedelta(days=1)
+
+    result = collection.insert_one(data)
+    object_id = result.inserted_id
 
     collection.update_one({'_id': object_id}, {'$set': {'file': filename}})
 
@@ -110,6 +113,9 @@ def properties_save_to_db(data, zpid):
     today = datetime.today().strftime('%Y-%m-%d')
     filename = f"{today}_{zpid}.csv"
     data['file'] = filename
+
+    data['createAt'] = datetime.now()
+    data['expireAt'] = datetime.now() + timedelta(days=1)
 
     existing_document = collection.find_one({'file': filename})
 
