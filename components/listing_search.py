b3fb168c4223c2f9b562b3b5e15a1cb45f15cc63
ChanyoungPark88 <chanyoung.p.1988@gmail.com>
2023-09-04 00:17:34 -0600

    Structure update

--- components/listing_search.py
+++ components/listing_search.py
@@ -5,7 +5,13 @@ This module fetches property listing details from a given URL,
 processes the data, and uploads it to Google Cloud Storage.
 """
 from library.libraries import st, pd, np
-from function.functions import get_listings, listings_save_to_db, gcs_connect, file_upload_to_gcs
+from function.functions import (
+    get_listings, listings_save_to_db,
+    gcs_connect, file_upload_to_gcs,
+    get_provinces_from_canada, get_cities_from_province,
+    get_states_from_usa, get_cities_from_state,
+    generate_zillow_url, download_location_file_from_gcs
+)
 
 
 def get_listing_info():
@@ -26,6 +32,67 @@ def get_listing_info():
         st.session_state.visibility = "visible"
         st.session_state.disabled = False
         st.session_state.placeholder = "Enter value"
+
+    country_list = ["Canada", "United States"]
+    selected_country_name = st.selectbox(
+        "Select a country", ["Select a country"] + country_list)
+
+    storage_client = gcs_connect()
+    if not storage_client:
+        st.error("Failed to connect to Google Cloud Storage.")
+
+    elif selected_country_name == "Canada":
+        file_name = "canadacities_selected.csv"
+        data_frame = download_location_file_from_gcs(file_name, storage_client)
+        if data_frame is not None:
+            # 주 선택 드롭다운 생성
+            provinces = ["Select a province"] + \
+                sorted(get_provinces_from_canada(data_frame))
+            selected_province = st.selectbox("Select a province", provinces)
+
+            # 선택된 주에 해당하는 도시 선택 드롭다운 생성
+            if selected_province != "Select a province":
+                cities = ["Select a city"] + \
+                    sorted(get_cities_from_province(
+                        data_frame, selected_province))
+                selected_city = st.selectbox("Select a city", cities)
+
+                if selected_city != "Select a city":
+                    city_data = data_frame[data_frame["city"]
+                                           == selected_city].iloc[0]
+                    city_lat = city_data['lat']
+                    city_lng = city_data['lng']
+                    province_id = city_data['province_id']
+
+                    zillow_url = generate_zillow_url(
+                        selected_city, province_id, city_lat, city_lng)
+                    st.write(f"Zillow URL: {zillow_url}")
+
+    elif selected_country_name == "United States":
+        file_name = "uscities_selected.csv"
+        data_frame = download_location_file_from_gcs(file_name, storage_client)
+        if data_frame is not None:
+            # 주(State) 선택 드롭다운 생성
+            states = ["Select a state"] + \
+                sorted(get_states_from_usa(data_frame))
+            selected_state = st.selectbox("Select a state", states)
+
+            # 선택된 주(State)에 해당하는 도시 선택 드롭다운 생성
+            if selected_state != "Select a state":
+                cities = ["Select a city"] + \
+                    sorted(get_cities_from_state(data_frame, selected_state))
+                selected_city = st.selectbox("Select a city", cities)
+
+                if selected_city != "Select a city":
+                    city_data = data_frame[data_frame["city"]
+                                           == selected_city].iloc[0]
+                    city_lat = city_data['lat']
+                    city_lng = city_data['lng']
+                    state_id = city_data['state_id']
+
+                    zillow_url = generate_zillow_url(
+                        selected_city, state_id, city_lat, city_lng)
+                    st.write(f"Zillow URL: {zillow_url}")
 
     # Container for entering the web link
     with st.container():
--- function/functions.py
+++ function/functions.py
@@ -16,6 +16,7 @@ HEADERS = {
 }
 
 BUCKET_NAME = os.environ.get('BUCKET_NAME')
+BUCKET_NAME2 = os.environ.get('BUCKET_NAME2')
 
 
 def gcs_connect():
@@ -155,7 +156,7 @@ def get_listings(listing_url):
         "api_key": API_KEY,
         "url": listing_url
     }
-    return requests.request("GET", url, params=querystring, headers=HEADERS)
+    return requests.request("GET", url, params=querystring, headers=HEADERS, timeout=20)
 
 
 def get_properties(zpid=None, address=None):
@@ -179,7 +180,7 @@ def get_properties(zpid=None, address=None):
         querystring['zpid'] = zpid
     if address is not None:
         querystring['address'] = address
-    return requests.request("GET", url, params=querystring, headers=HEADERS)
+    return requests.request("GET", url, params=querystring, headers=HEADERS, timeout=20)
 
 
 def listings_save_to_db(data):
@@ -309,6 +310,30 @@ def download_file_from_gcs(filename, storage_client, prefix, bucket_name=BUCKET_
     return data_frame
 
 
+def download_location_file_from_gcs(filename, storage_client, bucket_name=BUCKET_NAME):
+    """
+    Download a file from a Google Cloud Storage bucket.
+
+    Parameters:
+    - filename (str): The name of the file to download.
+    - storage_client (storage.Client): The Google Cloud Storage client.
+    - prefix (str): The folder prefix in the bucket.
+    - bucket_name (str, optional): The name of the bucket. Defaults to BUCKET_NAME.
+
+    Returns:
+    DataFrame or None: A DataFrame containing the downloaded data or None if the file doesn't exist.
+    """
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(filename)
+
+    if not blob.exists():
+        return None
+
+    content = blob.download_as_text()
+    data_frame = pd.read_csv(io.StringIO(content))
+    return data_frame
+
+
 def list_files_in_gcs(storage_client, prefix, bucket_name=BUCKET_NAME):
     """
     List all files in a specific prefix of a Google Cloud Storage bucket.
@@ -324,3 +349,103 @@ def list_files_in_gcs(storage_client, prefix, bucket_name=BUCKET_NAME):
     bucket = storage_client.get_bucket(bucket_name)
     blobs = bucket.list_blobs(prefix=prefix)
     return [blob.name.replace(f"{prefix}/", "") for blob in blobs]
+
+
+def get_states_from_usa(data_frame):
+    """
+    Retrieve unique states from the provided DataFrame.
+
+    Parameters:
+    - data_frame (DataFrame): DataFrame containing the data of cities in the USA.
+
+    Returns:
+    List: A list of unique states.
+    """
+    return data_frame["state_name"].unique().tolist()
+
+
+def get_provinces_from_canada(data_frame):
+    """
+    Retrieve unique provinces from the provided DataFrame.
+
+    Parameters:
+    - data_frame (DataFrame): DataFrame containing the data of cities in Canada.
+
+    Returns:
+    List: A list of unique provinces.
+    """
+    return data_frame["province_name"].unique().tolist()
+
+
+def get_cities_from_state(data_frame, state_name):
+    """
+    Retrieve cities for the specified state.
+
+    Parameters:
+    - df (DataFrame): DataFrame containing the data of cities in the USA.
+    - state_name (str): Name of the state to retrieve cities for.
+
+    Returns:
+    List: A list of cities for the specified state.
+    """
+    return data_frame[data_frame["state_name"] == state_name]["city"].tolist()
+
+
+def get_cities_from_province(data_frame, province_name):
+    """
+    Retrieve cities for the specified province.
+
+    Parameters:
+    - df (DataFrame): DataFrame containing the data of cities in Canada.
+    - province_name (str): Name of the province to retrieve cities for.
+
+    Returns:
+    List: A list of cities for the specified province.
+    """
+    return data_frame[data_frame["province_name"] == province_name]["city"].tolist()
+
+
+def generate_zillow_url(city, state_or_province, lat, lng):
+    """
+    Generate a Zillow search URL based on the given parameters.
+
+    Args:
+    - city (str): Name of the city.
+    - state_or_province (str): Name of the state or province.
+    - lat (float): Latitude of the city's center.
+    - lng (float): Longitude of the city's center.
+
+    Returns:
+    - str: A URL string for Zillow search based on the given parameters.
+    """
+    base_url = "https://www.zillow.com"
+
+    # 대략적인 지도의 경계값을 계산합니다 (예: +/-0.5도)
+    north = lat + 0.5
+    south = lat - 0.5
+    east = lng + 0.5
+    west = lng - 0.5
+
+    # URL 섹션을 별도로 구성합니다
+    url_path = f"{base_url}/{city.lower()}-{state_or_province.lower()}/"
+    query_pagination = "%7B%22pagination%22%3A%7B%7D%2C"
+    query_map_bounds = (f"%22mapBounds%22%3A%7B%22north%22%3A{north}%2C%22east%22%3A{east}%2C"
+                        f"%22south%22%3A{south}%2C%22west%22%3A{west}%7D%2C")
+    query_map_vis = "%22isMapVisible%22%3Atrue%2C"
+    query_filter_state = (
+        "%22filterState%22%3A%7B%22sort%22%3A%7B%22value%22%3A%22globalrelevanceex%22%7D%2C"
+        "%22ah%22%3A%7B%22value%22%3Atrue%7D%7D%2C"
+    )
+    query_list_vis = "%22isListVisible%22%3Atrue%7D"
+
+    # 섹션을 결합하여 최종 URL을 생성합니다
+    url = (
+        f"{url_path}?searchQueryState="
+        f"{query_pagination}"
+        f"{query_map_bounds}"
+        f"{query_map_vis}"
+        f"{query_filter_state}"
+        f"{query_list_vis}"
+    )
+
+    return url
